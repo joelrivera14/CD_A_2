2ef77900404c88159e2de71d2eb1e56739d0ab02
joel rivera <jorivera114@gmail.com>
2026-06-05 14:50:10 -0400

    bubbleSort

---
+++ bubbleSort.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void bubbleSort(std::vector<int> &arr);
+void print(std::vector<int> arr);
+
+int main()
+{
+    std::vector<int> nums = {23231, 1, 123, 2, 23, 2};
+    print(nums);
+    bubbleSort(nums);
+    print(nums);
+
+    return 0;
+}
+
+void bubbleSort(std::vector<int> &arr)
+{
+    int n = arr.size();
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n - 1; ++j)
+        {
+            if (arr[j] > arr[j + 1])
+                std::swap(arr[j], arr[j + 1]);
+        }
+    }
+}
+
+void print(std::vector<int> arr)
+{
+    for (auto i : arr)
+        std::cout << i << " ";
+    std::cout << "\n";
+}
